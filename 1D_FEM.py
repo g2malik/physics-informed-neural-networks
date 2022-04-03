2e315c0d1fd3c6bb6b46b726775179fe214befe6
g2malik <gagankumar2k@gmail.com>
2022-04-02 20:46:17 -0700

    1D NN innit commit

--- 1D_FEM.py
+++ 1D_FEM.py
@@ -37,8 +37,8 @@ plt.plot([(-width/2), (-width/2), (width/2), (width/2)], [0, length, length, 0])
 plt.xlim([(-width*2), (width*2)])
 plt.ylim([0, length*1.5])
 
-T_contour = np.reshape(T_profile, (len(T_profile), 1))
-plt.imshow(T_contour, extent = [-width/2, width/2, 0, length], origin='lower')
+T_contour = np.reshape(T_profile, (len(T_profile), 1)) 
+plt.imshow(T_contour, extent = [-width/2, width/2, 0, length], origin='lower', aspect='auto', interpolation='bilinear', cmap='YlOrRd')
 plt.colorbar()
 
 plt.show()
